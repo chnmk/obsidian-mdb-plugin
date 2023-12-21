3497a5185651092492e1dfece57bd3cdbb368504
chnmk <a2molchanov@ya.ru>
2023-12-21 16:51:58 +0300

    feat: add modal for file creation

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, Setting, Vault } from 'obsidian';
 
-// https://docs.obsidian.md/Plugins/Getting+started/Build+a+plugin
-// Remember to rename these classes and interfaces!
+// https://docs.obsidian.md/
+// Remember to rename these classes and interfaces! 
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -16,58 +16,23 @@ export default class HelloWorldPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
+	
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('file-volume', 'MusicDB Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('Hello World!');
+			new ExampleModal(this.app, (result) => {
+				new Notice(`Hello, ${result}!`);
+				this.app.vault.create(`${result}.md`, "test2")
+			  }).open()
+			
 		});
+
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		//ribbonIconEl.addClass('my-plugin-ribbon-class');
+ 		
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		//this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -92,22 +57,46 @@ export default class HelloWorldPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class ExampleModal extends Modal {
+	result: string;
+	onSubmit: (result: string) => void;
+  
+	constructor(app: App, onSubmit: (result: string) => void) {
+	  super(app);
+	  this.onSubmit = onSubmit;
 	}
-
+  
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	  const { contentEl } = this;
+  
+	  contentEl.createEl("h1", { text: "What's your name?" });
+  
+	  new Setting(contentEl)
+		.setName("Name")
+		.addText((text) =>
+		  text.onChange((value) => {
+			this.result = value
+		  }));
+  
+	  new Setting(contentEl)
+		.addButton((btn) =>
+		  btn
+			.setButtonText("Submit")
+			.setCta()
+			.onClick(() => {
+			  this.close();
+			  this.onSubmit(this.result);
+			}));
 	}
-
+  
 	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	  let { contentEl } = this;
+	  contentEl.empty();
 	}
-}
+  }
 
+
+/*
 class SampleSettingTab extends PluginSettingTab {
 	plugin: HelloWorldPlugin;
 
@@ -133,3 +122,4 @@ class SampleSettingTab extends PluginSettingTab {
 				}));
 	}
 }
+*/
