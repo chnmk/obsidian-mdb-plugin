3dd7d45bcbbad2d3c4dfe66eebc670f1a1ca8a51
chnmk <a2molchanov@ya.ru>
2023-12-22 22:42:04 +0300

    ref: rename default obsidian plugin classes

--- main.ts
+++ main.ts
@@ -1,83 +1,58 @@
-import { App, Modal, Notice, Plugin, Setting, Vault } from 'obsidian';
+import { App, Modal, Notice, Plugin, Setting } from 'obsidian';
 
 // https://docs.obsidian.md/
-// Remember to rename these classes and interfaces! 
+// https://docs.obsidian.md/Reference/TypeScript+API/Setting
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class HelloWorldPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class MDBPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-	
+
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('file-volume', 'MusicDB Plugin', (evt: MouseEvent) => {
+		this.addRibbonIcon('file-volume', 'MusicDB Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new ExampleModal(this.app, (result) => {
-				new Notice(`Hello, ${result}!`);
-				this.app.vault.create(`${result}.md`, "test2")
+			new MDBCreateNote(this.app, (noteName, noteDesc) => {
+				new Notice(`Note "${noteName}" created!`);
+				this.app.vault.create(`${noteName}.md`, noteDesc)
 			  }).open()
 			
 		});
-
-		// Perform additional things with the ribbon
-		//ribbonIconEl.addClass('my-plugin-ribbon-class');
- 		
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		//this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class ExampleModal extends Modal {
-	result: string;
-	onSubmit: (result: string) => void;
+class MDBCreateNote extends Modal {
+	noteName: string;
+	noteDesc: string;
+	onSubmit: (noteName: string, noteDesc: string) => void;
   
-	constructor(app: App, onSubmit: (result: string) => void) {
+	constructor(app: App, onSubmit: (noteName: string, noteDesc: string) => void) {
 	  super(app);
 	  this.onSubmit = onSubmit;
 	}
   
 	onOpen() {
-	  const { contentEl } = this;
-  
-	  contentEl.createEl("h1", { text: "What's your name?" });
-  
-	  new Setting(contentEl)
-		.setName("Name")
-		.addText((text) =>
-		  text.onChange((value) => {
-			this.result = value
-		  }));
-  
+		const { contentEl } = this;
+	
+		contentEl.createEl("h1", { text: "What's your name?" });
+	
+		new Setting(contentEl)
+			.setName("Name")
+			.addText((text) =>
+			text.onChange((value) => {
+				this.noteName = value
+			}));
+
+		new Setting(contentEl)
+			.setName("Description_temp")
+			.addText((text) =>
+			text.onChange((value) => {
+				this.noteDesc = value
+			}));
+	
 	  new Setting(contentEl)
 		.addButton((btn) =>
 		  btn
@@ -85,41 +60,12 @@ class ExampleModal extends Modal {
 			.setCta()
 			.onClick(() => {
 			  this.close();
-			  this.onSubmit(this.result);
+			  this.onSubmit(this.noteName, this.noteDesc);
 			}));
 	}
   
 	onClose() {
-	  let { contentEl } = this;
-	  contentEl.empty();
-	}
-  }
-
-
-/*
-class SampleSettingTab extends PluginSettingTab {
-	plugin: HelloWorldPlugin;
-
-	constructor(app: App, plugin: HelloWorldPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		let { contentEl } = this;
+		contentEl.empty();
 	}
 }
-*/
